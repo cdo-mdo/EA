d845543f4d9ca09d1192b320d6ca06571d2f6597
Manh Cuong Do <domanhcuong.cdo@gmail.com>
2024-11-30 15:15:10 -0600

    modify labw1d1

--- labw1d1/src/main/java/org/edu/miu/cs544/labw1d1/MainApplication.java
+++ labw1d1/src/main/java/org/edu/miu/cs544/labw1d1/MainApplication.java
@@ -6,33 +6,37 @@ import jakarta.persistence.Persistence;
 
 public class MainApplication {
     public static void main(String[] args) {
+        Car car1 = new Car("VF8", "VINFAST", 2024, 200000);
+        Car car2 = new Car("Camry", "TOYOTA", 2023, 100000);
+        Car car3 = new Car("Civic", "HONDA", 2022, 150000);
+        Car car4 = new Car("Ford Escape", "FORD", 2021, 200000);
+        Car car5 = new Car("Chevrolet", "GENERAL MOTOR", 2020, 180000);
+        Car car6 = new Car("X", "TESLA", 2024, 10000);
+
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("labw1d1");
         EntityManager em = emf.createEntityManager();
 
+        // Create
         em.getTransaction().begin();
-        Car car = new Car("VF8", "VINFAST", 2024, 200000);
-        Car car2 = new Car("Camry", "TOYOTA", 2024, 100000);
-        Car car3 = new Car("Civic", "HONDA", 2024, 150000);
-        Car car4 = new Car("Ford Escape", "FORD", 2024, 200000);
-        Car car5 = new Car("Chevrolet", "GENERAL MOTOR", 2024, 180000);
-        Car car6 = new Car("X", "TESLA", 2024, 10000);
 
-        em.persist(car);
+        em.persist(car1);
         em.persist(car2);
         em.persist(car3);
         em.persist(car4);
         em.persist(car5);
         em.persist(car6);
+
         em.getTransaction().commit();
 
-        Car carFromDB = em.find(Car.class, car.getId());
+        // Read
+        Car carFromDB1 = em.find(Car.class, car1.getId());
         Car carFromDB2 = em.find(Car.class, car2.getId());
         Car carFromDB3 = em.find(Car.class, car3.getId());
         Car carFromDB4 = em.find(Car.class, car4.getId());
         Car carFromDB5 = em.find(Car.class, car5.getId());
         Car carFromDB6 = em.find(Car.class, car6.getId());
         Car carFromDB7 = em.find(Car.class, car6.getId() + 1);
-        System.out.println(carFromDB);
+        System.out.println(carFromDB1);
         System.out.println(carFromDB2);
         System.out.println(carFromDB3);
         System.out.println(carFromDB4);
@@ -40,18 +44,21 @@ public class MainApplication {
         System.out.println(carFromDB6);
         System.out.println(carFromDB7);
 
-        car.setMileage(0);
-        Car merge = em.merge(car);
+        // Update
+        car1.setMileage(0);
+        Car merge = em.merge(car1);
         System.out.println(merge);
 
         em.getTransaction().begin();
-        carFromDB.setModel("VF3");
-        System.out.println(carFromDB);
-        System.out.println(car);
+        carFromDB1.setModel("VF3");
+        System.out.println(carFromDB1);
+        System.out.println(car1);
         em.getTransaction().commit();
 
+        // Delete
         em.getTransaction().begin();
         em.remove(car2);
+        em.remove(em.find(Car.class, car2.getId()));
         em.getTransaction().commit();
 
         em.close();
