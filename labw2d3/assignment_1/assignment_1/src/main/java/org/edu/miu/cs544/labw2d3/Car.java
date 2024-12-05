ca26bc9903bf1bf90042e64cb1fb32dd31f1b049
Manh Cuong Do <domanhcuong.cdo@gmail.com>
2024-12-04 20:36:13 -0600

    update

--- labw2d3/assignment_1/assignment_1/src/main/java/org/edu/miu/cs544/labw2d3/Car.java
+++
@@ -1,8 +0,0 @@
-package org.edu.miu.cs544.labw2d3;
-
-public class Car implements Vehicle {
-    @Override
-    public void move() {
-        System.out.println("moving at 50mph");
-    }
-}
--- labw2d3/assignment_2/assignment_2/src/main/java/org/edu/miu/cs544/labw2d3/Vehicle.java
+++
@@ -1,5 +0,0 @@
-package org.edu.miu.cs544.labw2d3;
-
-public interface Vehicle {
-    public void move();
-}
